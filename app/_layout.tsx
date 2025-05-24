16374aba0bf2a65ba206625e8d38e3e14b919823
vishal_chaure_ <144169854+vishal-chaure@users.noreply.github.com>
2025-05-24 10:42:09 +0530

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,29 @@
+// app/_layout.jsx
+// Import NativeWind setup first
+import '../nativewind-setup';
 import { Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import './global.css';
 
 export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="ARViewer" />
-      <Stack.Screen name="ARNavigation" />
-    </Stack>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 20);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  return <Stack />;
 }
